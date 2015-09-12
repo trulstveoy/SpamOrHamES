cbe0d31e2f0f06626db633714d4a03a3d7a7c6e4
trulstveoy <t@ruls.s>
2015-09-12 23:28:00 +0200

    file io as module

---
+++ src/file.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import fs from 'fs';
+
+export default function	read(filename) {
+	var fullname = path.join(__dirname, filename);
+	var content = fs.readFileSync(fullname, 'utf8');
+	var lines = content.split('\n');
+	var messages = lines.filter(line => 
+		line.trim() !== ''
+	).map(line => {			
+		var tokens = line.split('\t');
+		var label = tokens[0];
+		var text = tokens[1];
+		
+		if(!label || !text){
+			console.log('line cannot be parsed: ' + line);
+		}						
+		
+		return {label: label, text: text }
+	});
+	
+	return messages;
+}
--- src/main.js
+++ src/main.js
@@ -1,24 +1,8 @@
-import path from 'path';
-import fs from 'fs';
+import read from './file';
 
 console.log('start');
 
-var fullname = path.join(__dirname, '../data/messages.txt');
-var content = fs.readFileSync(fullname, 'utf8');
-var lines = content.split('\n');
-var messages = lines.filter(line => 
-	line.trim() !== ''
-).map(line => {			
-	var tokens = line.split('\t');
-	var label = tokens[0];
-	var text = tokens[1];
-	
-	if(!label || !text){
-		console.log('line cannot be parsed: ' + line);
-	}						
-	
-	return {label: label, text: text }
-});
+let messages = read('../data/messages.txt');
 
 var classify = function(text){
 	if(text.toLowerCase().indexOf('free') === -1){	
