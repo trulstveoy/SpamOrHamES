4f4d9251ac4b4aee0aa6a89917682b93f8faa7a8
trulstveoy <t@ruls.s>
2015-10-15 22:11:07 +0200

    replace deleted items

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var minimist = require('minimist');
+var del = require('del');
+
+var knownOptions = {
+  string: 'dir',
+  default: { dir: 'src' }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+var source = options.dir + '/**/*.js';
+
+gulp.task('clean', function() {
+  return del(['lib']);
+});
+
+gulp.task('build', function () {
+    return gulp.src(source)        
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(babel({ 
+            modules: 'common',
+            stage: 0,
+            optional: [
+                'es7.asyncFunctions',
+                'runtime'
+            ],
+            externalHelpers: true,
+        }))        
+        .pipe(sourcemaps.write('.', {sourceRoot: '../' + options.dir}))
+        .pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch', function() {
+   gulp.watch(source, ['build']);
+});
